080a8ac880a6807018f759e772b180aebff45880
Pranav Dhangar <113121206+pranavv04@users.noreply.github.com>
2026-01-18 23:02:50 +0530

    Version 1.0.1

--- Orchestra.Worker/Program.cs
+++ Orchestra.Worker/Program.cs
@@ -5,10 +5,10 @@ using Orchestra.Worker.Services;
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
-        // Register HttpClient for WorkerService
+        
         services.AddHttpClient<WorkerService>();
 
-        // Register WorkerService as a hosted service
+        
         services.AddHostedService<WorkerService>();
     })
     .Build();
